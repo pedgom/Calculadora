05267a4575fc5d0f0f97731231681498bf03a285
Pedro Gomes <aluno21279@ipt.pt>
2021-03-16 00:50:34 +0000

    Construção da View

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -23,7 +23,8 @@ namespace Calculadora.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult Index(string visor)
         {
             return View();
         }
